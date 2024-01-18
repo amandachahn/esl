8e3005e26e428d38e75e03c27ba8058e83706f88
amandachahn <156614568+amandachahn@users.noreply.github.com>
2024-01-18 12:21:57 -0800

    add slang

--- process/idiom_load.R
+++ process/idiom_load.R
@@ -8,10 +8,23 @@ There are 792!
 
 No way to go through them all in one class.
 Below, we load and randomly sample some to use in scenes and discuss.
+
+
+Slang is pulled from across the web.
+https://www.berlitz.com/blog/american-slang-words
+https://www.umass.edu/ipo/sites/default/files/slang.pdf
+https://www.salisbury.edu/administration/academic-affairs/center-for-international-education/international-student-scholar-services/handbook/slang.aspx
+https://amberstudent.com/blog/post/popular-american-slang-words
+
+There are 1,022 idioms and slang to choose from below! It's a lot. Which makes the random selection helpful.
 "
 
-# load the sheet
+# load the sheets
 idioms <- read.csv2("assets/slang/idioms_from_intheloop.csv", header = FALSE)
+slang <- read.csv2("assets/slang/slang.csv",header = FALSE)
 
-# randomly select some idioms!
-sample_n(idioms, 3)
+# combine
+american.lang <- bind_rows(idioms,slang)
+
+# randomly select!
+sample_n(american.lang, 3)
